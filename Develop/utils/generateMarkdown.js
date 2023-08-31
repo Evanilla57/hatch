d350e00b059d31f50ce5738d62accf83e1f15553
Evanilla57 <grigg005@cougars.csusm.edu>
2023-08-30 20:00:25 -0700

    added template literal to generateMarkdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,7 +13,49 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [Tests](#tests)
+  - [Contributing](#contributing)
+  - [Questions](#questions)
+  - [License](#license)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Credits
+  
+  ${data.credits}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Questions
+  
+  Email Address: ${data.email}
+  GitHub Username:  ${data.git}
+  
+  ## License
+  
+  ${data.license}
 `;
 }
 
